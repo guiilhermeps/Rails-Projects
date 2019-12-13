d485e8c2148c45e9acbb9290ef5768f43921f30b
Guilherme <guisil_ps@hotmail.com>
2019-12-13 15:24:50 -0300

    Vendo um pouco mais sobre rotas e aplicando alguns exemplos

--- modulo_03/crypto_wallet/app/controllers/welcome_controller.rb
+++ modulo_03/crypto_wallet/app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @nome = params.has_key?(:nome) ? params[:nome] : "User"
   end
 end
--- modulo_03/crypto_wallet/app/helpers/application_helper.rb
+++ modulo_03/crypto_wallet/app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def data_br(data_usa)
+    data_usa.strftime("%d/%m/%Y")
+  end
+
+  def nome_aplicacao
+    "Crypto Wallet APP"
+  end
+
+  def ambiente_rails
+    if Rails.env.development?
+      "Desenvolvimento"
+    elsif Rail.env.production?
+      "Produção"
+    else
+      "Teste"
+    end
+  end
 end
